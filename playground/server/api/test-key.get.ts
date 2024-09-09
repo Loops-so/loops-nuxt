6137e59738091b9b1a503ef077b5eb39633c9a25
Dan <dan@loops.so>
2024-09-09 13:16:34 +0300

    Test API key example endpoint

--- playground/server/api/test-key.get.ts
+++ playground/server/api/test-key.get.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const { loops } = event.context
 
-  const response = await loops.getCustomFields()
-  console.log(response)
+  const response = await loops.testApiKey()
+  return response
 })
